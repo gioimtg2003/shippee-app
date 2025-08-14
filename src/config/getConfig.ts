082b29671e1f15dfaa0b101e5e414c3400b62251
Nguyen Cong Gioi <conggioi.pro264@gmail.com>
2025-08-14 09:10:12 +0700

    chore: up

--- src/config/getConfig.ts
+++ src/config/getConfig.ts
@@ -9,7 +9,7 @@ interface IEnvironmentConfig {
 const EnvironmentConfig: IEnvironmentConfig = {
   local: {
     appUrl: 'http://localhost:3001',
-    apiUrl: ' http://localhost:5000/api',
+    apiUrl: ' http://192.168.1.150:5001/api',
     WsUrl: 'ws://localhost:4000',
   },
 
